433b132e91c28dd085fa8b82a723015567197b9a
Dr-Kohl <kohlc@cedarville.edu>
2026-09-04 14:17:19 -0400

    Make Canoe Layout Planner the default site

--- pages-entry.tsx
+++ pages-entry.tsx
@@ -5,7 +5,7 @@ import './app/canoe.css';
 import Home from './app/page';
 import LayoutPlanner from './app/layout-planner/page';
 
-const showPlanner = new URLSearchParams(window.location.search).get('tool') === 'layout';
+const showPlanner = new URLSearchParams(window.location.search).get('tool') !== 'float';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>{showPlanner ? <LayoutPlanner /> : <Home />}</StrictMode>,
